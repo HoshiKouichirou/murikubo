322139058a8fa6fec0e022b14bc202bb59028841
poyo_gamer <32663150+poyo555@users.noreply.github.com>
2018-05-25 10:34:45 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+$(function() {
+  for (var i = 0; i <= 15; i++) {
+    console.log("hi")
+    $(".box").append("<div class='muri " + i + "'>むりくぼぉぉ。。。</div>")
+    Random($("." + i + ""))
+  }
+  function Random(test){
+    var start = Math.round( Math.random()*10 );
+    var x = Math.round( Math.random()*100 );
+    var y = Math.round( Math.random()*100 );
+    test.css("animation-delay", "" + start + "s");
+    test.css("transform", "translate(" + x + "vw," + y + "vh)");
+    console.log()
+  }
+}())
